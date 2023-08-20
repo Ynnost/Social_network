5fa0a75dd02beb688594d0b7fafa1dae0e3b5416
Ynnost <ynnost94@gmail.com>
2023-08-20 23:51:51 +0300

    The beginning of social network layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://yt3.googleusercontent.com/ytc/AGIKgqMWG9oD8d4m7xqT8U7HyPit6oDFdcBShzO-9RgK=s900-c-k-c0x00ffffff-no-rj'  alt={""}/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img className='img-obl'
+                     src="https://sportishka.com/uploads/posts/2022-11/1667538859_10-sportishka-com-p-avtomobil-bochka-oboi-11.jpg" alt={""}/>
+            </div>
+            <div> ava + description</div>
+            <div> My post <div>New Post <div>
+                <div>Post 1</div>
+                <div>Post2</div>
+            </div></div></div>
+        </div>
+    );
+};
+
+export default Profile;
